a9f687b5db2169f27fe4100698c404a10d2dd0be
Linus Johansson <linus@ontheline.se>
2026-07-21 19:19:57 +0200

    Adding Employee

--- business/models.py
+++ business/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+from common.models import AuditModel
+
+class Employee(AuditModel):
+    name = models.CharField(max_length=100, null=False, blank=False)   
