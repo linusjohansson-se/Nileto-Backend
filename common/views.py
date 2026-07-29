bd84f7421fcac2e0ad13e284b358013e65629bcc
Linus Johansson <linus@ontheline.se>
2026-07-29 16:54:17 +0200

    Introduce StandardModelViewSet for common DRF configurations
    - Create an abstract `StandardModelViewSet` in `common/views.py` to centralize default Django REST Framework settings: `filterset_fields = "__all__"`, `ordering_fields = "__all__"`, and `ordering = ["id"]`.
    - Refactor `ContactViewSet`, `ContactPhoneViewSet`, and `CustomerViewSet` to inherit from this new base class, removing duplicated configuration lines from each viewset.
    - This promotes code reusability, enhances consistency across API viewsets, and simplifies the creation of future viewsets with standard filtering and ordering capabilities.

--- common/views.py
+++ common/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+class StandardModelViewSet(viewsets.ModelViewSet):
+    filterset_fields = "__all__"
+    ordering_fields = "__all__"
+    ordering = ["id"]
+
+    class Meta:
+        abstract = True
--- customers/views.py
+++ customers/views.py
@@ -1,28 +1,21 @@
 from rest_framework import viewsets, filters
 
+from common.views import StandardModelViewSet
+
 from .models import Contact, ContactPhone, Customer
 from .serializers import ContactPhoneSerializer, ContactSerializer, CustomerSerializer
 
-class ContactViewSet(viewsets.ModelViewSet):
+class ContactViewSet(StandardModelViewSet):
     serializer_class = ContactSerializer
     queryset = Contact.objects.prefetch_related("emails", "phone_numbers")
     search_fields = ["name", "emails__email", "phone_numbers__phone_number", "=id"]
-    filterset_fields = "__all__"
-    ordering_fields = "__all__"
-    ordering = ["id"]
 
-class ContactPhoneViewSet(viewsets.ModelViewSet):
+class ContactPhoneViewSet(StandardModelViewSet):
     serializer_class = ContactPhoneSerializer
     queryset = ContactPhone.objects.all()
     search_fields = ["phone_number", "=id"]
-    filterset_fields = "__all__"
-    ordering_fields = "__all__"
-    ordering = ["id"]
 
-class CustomerViewSet(viewsets.ModelViewSet):
+class CustomerViewSet(StandardModelViewSet):
     serializer_class = CustomerSerializer
     queryset = Customer.objects.prefetch_related("addresses")
     search_fields = ["name", "=id"]
-    filterset_fields = "__all__"
-    ordering_fields = "__all__"
-    ordering = ["id"]
